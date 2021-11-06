ac8ca91849c449a84d0385c5c704250785eeda11
unknown <jamzdeveloper@gmail.com>
2021-11-06 17:38:05 -0500

    actualizado gitignore

--- api/dist/controllers/registrarInvestigador.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=registrarInvestigador.js.map
--- api/dist/routes/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var login_1 = require("../controllers/login");
-var router = (0, express_1.Router)();
-router.get("/", login_1.getLogin);
-exports["default"] = router;
-//# sourceMappingURL=login.js.map
